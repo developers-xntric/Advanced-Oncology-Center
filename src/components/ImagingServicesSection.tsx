e5e0696802835464b288198a242610910669f507
AbduSubhan11 <160301116+AbduSubhan11@users.noreply.github.com>
2026-04-13 13:21:16 +0500

    completed by subhan😎

--- src/components/ImagingServicesSection.tsx
+++ src/components/ImagingServicesSection.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 export default function ImagingServicesSection() {
   const services = [
     "Nuclear Medicine",
@@ -10,34 +12,35 @@ export default function ImagingServicesSection() {
   ];
 
   return (
-    <section className="relative w-full overflow-hidden py-20 ">
+    <section className="relative w-full overflow-hidden py-16 md:py-20 ">
       {/* Background Image */}
       <div className="absolute inset-0">
-        <img
+        <Image
+          fill
           src="/imaging-services-bg.png"
           alt="Medical imaging services"
           className="h-full w-full object-cover"
         />
-       
+
       </div>
 
       {/* Content */}
-      <div className="relative z-10 mx-auto  w-[90%]">
-        <div className="flex items-center justify-between gap-28">
+      <div className="relative z-10 mx-auto 2xl:max-w-[1580px]  w-[90%] md:w-[95%]">
+        <div className="flex  lg:flex-row flex-col items-center justify-between lg:gap-28 gap-10">
           {/* Left Content */}
-          <div className="flex flex-col justify-center w-[50%]">
+          <div className="flex flex-col justify-center lg:w-[50%]">
             {/* Small heading */}
             <p className="mb-4 text-sm font-medium uppercase tracking-wider text-white">
               Advanced Imaging Services
             </p>
 
             {/* Main heading */}
-            <h2 className="mb-4 text-3xl font-medium leading-tight text-white md:text-4xl 2xl:text-5xl">
+            <h2 className="mb-4 text-[28px] md:text-3xl font-medium leading-tight text-white md:text-4xl 2xl:text-5xl">
               Precision Diagnostics. Clearer Insights. Better Care.
             </h2>
 
             {/* Description */}
-            <p className="text-base leading-relaxed text-white/80 2xl:text-lg">
+            <p className=" text-[14px] md:text-base leading-relaxed text-white 2xl:text-lg">
               Imaging is more than technology. It's the foundation of accurate diagnosis and
               personalised treatment. From PET/CT scans to MRI and ultrasound, our advanced systems
               provide clear, reliable results that guide every step of your care journey.
@@ -45,7 +48,7 @@ export default function ImagingServicesSection() {
           </div>
 
           {/* Right Content - Services */}
-          <div className="flex flex-col justify-center w-[50%]">
+          <div className="flex flex-col justify-center lg:w-[50%]">
             {/* Section title */}
             <h3 className="mb-6 text-xl font-semibold text-white md:text-2xl">
               Imaging Services
